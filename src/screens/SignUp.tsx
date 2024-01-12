efa85990af193aa91b408ad9cff9f98bcfc1bf6a
carlosgodoi <edu-godoi@hotmail.com>
2024-01-12 18:24:43 -0300

    Iniciando integração com a API

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -2,12 +2,15 @@ import { VStack, Image, Center, Heading, Text, ScrollView } from "native-base";
 import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
+import { api } from "@services/api";
+import axios from "axios";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useNavigation } from "@react-navigation/native";
+import { Alert } from "react-native";
 
 const signUpSchema = yup.object({
   name: yup.string().required("Nome obrigatório"),
@@ -39,9 +42,25 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp({ name, email, password, password_confirm }: FormData) {
-    console.log({ name, email, password, password_confirm });
+  async function handleSignUp({
+    name,
+    email,
+    password,
+    password_confirm,
+  }: FormData) {
+    try {
+      const response = await api.post("/users", {
+        name,
+        email,
+        password,
+        password_confirm,
+      });
+      console.log(response.data);
+    } catch (error) {
+      if (axios.isAxiosError(error)) Alert.alert(error.response?.data.message);
+    }
   }
+
   return (
     <ScrollView
       contentContainerStyle={{ flexGrow: 1 }}
